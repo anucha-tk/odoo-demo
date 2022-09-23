33eef1477b584c0c32e018901440e90284656ee1
Anucha.tk <anuchataowkaen@gmail.com>
2022-09-23 17:27:13 +0700

    add module and image

---
+++ addons/order_in/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    "name": "Order In",
+    "version": "1.0.0",
+    "author": "anucha.tk",
+    "sequence": -100,
+    "category": "Order",
+    "summary": "Order In management system",
+    "description": """Order In management system""",
+    "depends": [],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}

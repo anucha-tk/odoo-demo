4a8cbe927a9bae2e4594d327d02827e5837f5dc7
Anucha.tk <anuchataowkaen@gmail.com>
2022-10-02 15:36:19 +0700

    tracking

--- addons/order_in/models/customer.py
+++ addons/order_in/models/customer.py
@@ -6,8 +6,8 @@ class OrderCustomer(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Customer"
 
-    name = fields.Char(string="Name", required=True)
-    address = fields.Char(string="Address", required=True)
-    phone = fields.Char(string="Phone", required=True)
-    email = fields.Char(string="Email", required=True)
+    name = fields.Char(string="Name", required=True, tracking=True)
+    address = fields.Char(string="Address", required=True, tracking=True)
+    phone = fields.Char(string="Phone", required=True, tracking=True)
+    email = fields.Char(string="Email", required=True, tracking=True)
     active = fields.Boolean(string="Active", default=True)

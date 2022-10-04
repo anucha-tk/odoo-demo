2efebe03b68500356e8437257f4e74ba3f6c75ed
Anucha.tk <anuchataowkaen@gmail.com>
2022-10-04 16:16:12 +0700

    default date

--- addons/order_in/models/appointment.py
+++ addons/order_in/models/appointment.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from odoo import models, fields, api
 
 
@@ -7,5 +8,5 @@ class OrderAppointment(models.Model):
     _description = "Order Appointment"
 
     customer_id = fields.Many2one("order.customer", string="Customer", required=True)
-    appointment_time = fields.Datetime(string="Appointment Time", required=True)
-    booking_date = fields.Date(string="Booking Date", required=True)
+    appointment_time = fields.Datetime(string="Appointment Time", required=True, default=fields.Datetime.now)
+    booking_date = fields.Date(string="Booking Date", required=True, default=fields.Date.context_today)

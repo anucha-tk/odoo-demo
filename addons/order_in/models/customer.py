a215cfabf64662a79492d1bdf7e91626b6a12f59
Anucha.tk <anuchataowkaen@gmail.com>
2022-10-07 14:52:37 +0700

    compute

--- addons/order_in/models/customer.py
+++ addons/order_in/models/customer.py
@@ -1,5 +1,5 @@
+from datetime import date
 from odoo import models, fields, api
-
 
 class OrderCustomer(models.Model):
     _name = "order.customer"
@@ -7,6 +7,8 @@ class OrderCustomer(models.Model):
     _description = "Customer"
 
     name = fields.Char(string="Name", required=True, tracking=True)
+    date_of_birth = fields.Date(string="Date Of Birth")
+    age = fields.Integer(string="Age", compute="_compute_age", tracking=True)
     address = fields.Char(string="Address", required=True, tracking=True)
     phone = fields.Char(string="Phone", required=True, tracking=True)
     email = fields.Char(string="Email", required=True, tracking=True)
@@ -19,3 +21,12 @@ class OrderCustomer(models.Model):
         ],
         string="customer_type",
     )
+
+    @api.depends("date_of_birth")
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0

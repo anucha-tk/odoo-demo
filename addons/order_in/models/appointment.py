107b81af39c19d6b5004d9a68d9527fe116c969d
Anucha.tk <anuchataowkaen@gmail.com>
2022-10-08 10:49:12 +0700

    rec_name

--- addons/order_in/models/appointment.py
+++ addons/order_in/models/appointment.py
@@ -5,6 +5,7 @@ class OrderAppointment(models.Model):
     _name = "order.appointment"
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Order Appointment"
+    _rec_name = "customer_id"
 
     customer_id = fields.Many2one("order.customer", string="Customer", required=True)
     appointment_time = fields.Datetime(string="Appointment Time", required=True, default=fields.Datetime.now)

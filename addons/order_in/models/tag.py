3e2ebdd112d94afbc4b21be171c60f1b11dc733a
Anucha.tk <anuchataowkaen@gmail.com>
2022-10-21 11:50:06 +0700

    duplicate unique name

--- addons/order_in/models/tag.py
+++ addons/order_in/models/tag.py
@@ -12,3 +12,13 @@ class Tag(models.Model):
     _sql_constraints = [
         ("unique_tag_name", "unique (name)", "Name must be unique."),
     ]
+
+    @api.returns("self", lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+
+        if not default.get("name"):
+            default["name"] = self.name + " (copy)"
+
+        return super(Tag, self).copy(default)
